2c57b1698bcf806955dd8026ad270340177d89b2
christopher mikkelsen <christophermikkelsen@live.dk>
2024-12-18 08:40:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GF2_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
